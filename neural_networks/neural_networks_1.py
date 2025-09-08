3efab5a4c4d74394d5309bf1b5f3801a072fbc27
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-09-08 01:29:39 -0300

    Create neural_networks_1.py

---
+++ neural_networks/neural_networks_1.py
@@ -0,0 +1,18 @@
+inputs = [1, 2, 3, 2.5]
+
+wheigts = [[0.342, -0.876, 0.123, 0.567],
+           [-0.234, 0.789, -0.456, 0.901],
+           [0.654, -0.321, 0.987, -0.543]]
+
+biases = [2, 3, 0.5]
+
+layer_output = []
+
+for neuron_wheigts, neuron_bias in zip(wheigts, biases):
+    neuron_output = 0
+    for temp_input, wheigt in zip(inputs, neuron_wheigts):
+        neuron_output += temp_input*wheigt
+    neuron_output += neuron_bias
+    layer_output.append(neuron_output)
+    
+print(layer_output)
