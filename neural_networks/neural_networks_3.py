b01709050f0f444bd6de3642e6792c12d4c74936
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-09-10 17:01:48 -0300

    Started layer functions

---
+++ neural_networks/neural_networks_3.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def dense_layer(self, n_inputs, n_neurons):
+    # Initialaze weights and biases
+
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    
+    pass # Placeholder
+
+def forward(self, inputs):
+    # Calculate output values from inputs, weights and biases
+    pass # Placeholder
+
