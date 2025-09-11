9db0d89c4c359fa5fd6e7b59c81f2f43fc703ec9
emiliano-gandini-outeda <emiliano.outeda@gmail.com>
2025-09-10 22:38:29 -0300

    Fixed erros

--- neural_networks/neural_networks_3.py
+++ neural_networks/neural_networks_3.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-def dense_layer(self, n_inputs, n_neurons):
+class dense_layer:
     # Initialaze weights and biases
 
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        
+    def forward(self, inputs):
+        # Calculate output values from inputs, weights and biases
+        pass # Placeholder
     
     pass # Placeholder
 
-def forward(self, inputs):
-    # Calculate output values from inputs, weights and biases
-    pass # Placeholder
+
 
